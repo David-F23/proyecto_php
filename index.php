f387989c7fe725d608fcad29f228043247a6359a
David E. Flores <floresdavide0@gmail.com>
2021-04-15 16:13:32 -0600

    Se agrego un formulario

--- index.php
+++ index.php
@@ -1,12 +1,24 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Formulario</title>
 </head>
 <body>
     
+    <fieldset>
+        <legend>Formulario</legend>
+        <form action="#" method="post">
+            <label for="nombre">Nombre Completo</label><br>
+            <input type="text" name="nombre" id="nombre"><br>
+            <label for="teléfono">Teléfono</label><br>
+            <input type="number" name="teléfono" id="teléfono"><br>
+            <label for="direccion">Dirección</label><br>
+            <input type="text" name="direccion" id="direccion"><hr>
+            <input type="submit" value="Enviar">
+        </form>
+    </fieldset>
 </body>
 </html>
